e4326d32f177314b8e9a7911e1e5c4d9c7f1cbd9
Victoria Godsoe <victoria.godsoe@amd.com>
2025-09-16 12:45:08 -0700

    Add AMD copyright to scripts, include License file

--- lira/utils/tokens.py
+++ lira/utils/tokens.py
@@ -1,3 +1,5 @@
+# Copyright(C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+ 
 def load_tokens(token_path):
     with open(token_path, "r", encoding="utf-8") as f:
         return [line.strip().split()[0] for line in f]
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Copyright(C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+
 import os
 import gradio as gr
 import torchaudio
